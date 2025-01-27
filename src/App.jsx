25d67cd4915633c45eb26ffd08e470f63e4e0e22
Lisa Mrc <lisa.muracciole77@gmail.com>
2025-01-27 16:57:38 +0100

    [DONE] Calculated data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,53 @@
-import './App.css';
-import dogsData from './dogsData';
-import DogCard from './components/DogCard';
-import newsData from './newsData';
-import NewsSlider from './components/NewsSlider';
+import "./App.css";
+import { useState, useMemo } from "react";
+import dogsData from "./dogsData";
+import NewsSlider from "./components/NewsSlider";
+import DogCard from "./components/DogCard";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+  const [dogsSortBy, setDogsSortBy] = useState("age");
+
+  const filteredDogsData = useMemo(() => {
+    let result = dogsData.filter((dog) =>
+      dog.name.toLowerCase().includes(search.toLowerCase())
+    );
+    result = result.toSorted((a, b) => {
+      if (dogsSortBy === "age") {
+        // age can be null
+        return (a.age || 0) - (b.age || 0);
+      } else {
+        // sort in alphabetical order
+        return a.name.localeCompare(b.name);
+      }
+    });
+    return result;
+  }, [dogsData, search, dogsSortBy]);
+
   return (
     <div>
-        <NewsSlider
-            title={newsData.title}
-        />
       <h1>Dog Center</h1>
+      <NewsSlider />
+      <div id="gallery-options">
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search dog"
+        />
+        <label htmlFor="dog-sort">Sort by : </label>
+        <select
+          id="dog-sort"
+          value={dogsSortBy}
+          onChange={(e) => setDogsSortBy(e.target.value)}
+        >
+          <option value="age">Age</option>
+          <option value="name">Name</option>
+        </select>
+      </div>
       <div id="dog-gallery">
-        { dogsData.map((dog) => (
-        <DogCard 
+        {filteredDogsData.map((dog) => (
+          <DogCard
             key={dog.id}
             name={dog.name}
             age={dog.age}
