7d564e2cef142b79e72d17ca96f85a515e55f28b
Lisa Mrc <lisa.muracciole77@gmail.com>
2025-10-28 18:10:59 -0400

    🚀 Ready to launch on Vercel !

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({}) {
       <div className="navbar-left">
         <a href="/">
           <img
-            src="../assets/Icons/favicon.svg"
+            src="/assets/Icons/favicon.svg"
             alt="Morfale Logo"
             className="logo"
           ></img>
@@ -15,12 +15,8 @@ export default function Navbar({}) {
       <div className="navbar-right">
         <a href="/favorites" className="nav-item">
           <span className="nav-text">Favorite recipes</span>
-          <img src="../assets/Icons/heart.svg" alt="Heart Icon"></img>
+          <img src="/assets/Icons/heart.svg" alt="Heart Icon"></img>
         </a>
-        {/* <a href="#" className="nav-item">
-          <span className="nav-text">Random meals</span>
-          <img src="../assets/Icons/repeat.svg" alt="Shuffle Icon"></img>
-        </a> */}
       </div>
     </nav>
   );
